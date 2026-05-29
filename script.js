233ffcbcec5646163ce85aa926bee8cc12fdcef3
Zach Raper <raperzac05@gmail.com>
2026-05-29 16:33:14 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const fadeElements = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) 
+        {
+            entry.target.classList.add("show");
+            observer.unobserve(entry.target);
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+fadeElements.forEach((el) => {
+    observer.observe(el);
+});
